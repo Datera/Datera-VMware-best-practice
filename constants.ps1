895e6c48109d47958f572e58bf42ebb6d660d108
Virgil Mihailovici <virgil.mihailovici@datera.io>
2019-09-11 09:46:53 -0700

    Pull request for Morae changes.
    Major rewrite. Many changes:
    - split helper functions and constants
    - rewrite from KJ (Morae)
    - new parameters
    - support for SMTP alerts
    - several output formats
    - fixing MacOS related issues with Write-PSObject
    - fixing MacOS related sort alias
    - updated README

---
+++ constants.ps1
@@ -0,0 +1,13 @@
+########
+########  Script Constants
+########
+
+## Storage Values
+
+    $DateraIscsiQueueDepth = '16' # Datera recommends 16
+
+## Email
+
+    $SMTP_RELAY = "smtp.example.com"
+    $SMTP_FROM = "datera-alerts@example.com"
+    $SMTP_TO = "alerts@example.com"
