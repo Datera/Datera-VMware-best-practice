f0c4d89f9a7a875b6a73a05c4725a29858fee412
Virgil <virgilm@gmail.com>
2020-06-12 15:51:07 -0700

    Add a new check/fix for AQD (Automatic Queue Depth).
    Mode details here:  https://kb.vmware.com/s/article/1008113
    Refer to #CET-512
    Other cosmetic fixes:
    - reorganized script parameter section
    - reorganized field names in the results table
    - reorganized field matching/coloring in the output table

--- constants.ps1
+++ constants.ps1
@@ -5,7 +5,9 @@
 ## Storage Values
 
     $DateraIscsiQueueDepth = '16' # Datera recommends 16
-
+    $DateraLunQueueFullSampleSize = '32'
+    $DateraLunQueueFullThreshold = '4'
+    # Reference https://kb.vmware.com/s/article/1008113
 ## Email
 
     $SMTP_RELAY = "smtp.example.com"
